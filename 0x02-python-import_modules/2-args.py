f771dd6e289091f238f77f4a1f084eaff6a0f169
LeighAnne17 <leighndimande17@icloud.com>
2024-03-14 16:51:14 -0700

    changes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 import sys
-if __name__ = "__main__":
-    args = sys.argv[1:]
-    num_args = len(args)
-    print("Number of argument{}: {}".format("s" if num_args != 1 else "", num_args)< end="")
-    print("{}".formnat("s"if num_args != 1 else ""), end="")
-    print("{}".format(":" if num_args != 0 else "."))
+args = sys.argv[1:]
+num_args = len(args)
+print("Number of argument{}: {}".format("s" if num_args != 1 else "", num_args)< end="")
+print("{}".formnat("s"if num_args != 1 else ""), end="")
+print("{}".format(":" if num_args != 0 else "."))
     
-    if num_args >0:
+    if num_args > 0:
         foor i, arg in enumerate(args, 1):
             print("{}: {}".format(i, arg))
 
