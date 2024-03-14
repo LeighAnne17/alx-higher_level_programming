a33cf0e8162ce0a1f87bf02542e7763e4a53bb95
LeighAnne17 <leighndimande17@icloud.com>
2024-03-14 15:56:15 -0700

    Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import sys
 from calculator_1.py import add, sub, mul, div
-if __name ="__main__":
     if len(sys.argv) != 4:
         print ("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
