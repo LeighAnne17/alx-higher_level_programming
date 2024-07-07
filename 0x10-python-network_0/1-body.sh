2be548b5451fb04e555de2813a170485b673b15c
LeighAnne17 <leighndimande17@icloud.com>
2024-07-07 07:53:39 -0700

    network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request to a URL and displays the body of a 200 status code response
-curl -sL -X GET "$1" -o temp.txt && [ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" -eq 200 ] && cat temp.txt
+curl -sL -X GET "$1" -o /dev/null && curl -sL "$1"
