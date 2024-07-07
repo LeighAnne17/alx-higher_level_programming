1e7e435de39f89ac37711824e75d04ef736752ac
LeighAnne17 <leighndimande17@icloud.com>
2024-07-07 08:23:37 -0700

    python network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,6 +5,16 @@ Finds a peak in a list of unsorted integers using binary search approach.
 """
 
 def find_peak(list_of_integers):
+    """
+    Finds a peak element in a list of unsorted integers.
+    A peak element is defined as an element that is not smaller than its neighbors.
+
+    Args:
+        list_of_integers (list): A list of unsorted integers.
+
+    Returns:
+        int: A peak element or None if the list is empty.
+    """
     if not list_of_integers:
         return None
     
@@ -19,4 +29,3 @@ def find_peak(list_of_integers):
             left = mid + 1
     
     return list_of_integers[left]
-
