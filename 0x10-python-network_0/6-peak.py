06e4440525ba523218623a31d934538862cfa9a9
LeighAnne17 <leighndimande17@icloud.com>
2024-07-07 08:17:12 -0700

    network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
+"""
+Function find_peak:
+Finds a peak in a list of unsorted integers using binary search approach.
+"""
 
 def find_peak(list_of_integers):
-    """Find a peak in a list of unsorted integers"""
     if not list_of_integers:
         return None
+    
     left, right = 0, len(list_of_integers) - 1
+    
     while left < right:
         mid = (left + right) // 2
-        if list_of_integers[mid] < list_of_integers[mid + 1]:
-            left = mid + 1
-        else:
+        
+        if list_of_integers[mid] >= list_of_integers[mid + 1]:
             right = mid
+        else:
+            left = mid + 1
+    
     return list_of_integers[left]
+
