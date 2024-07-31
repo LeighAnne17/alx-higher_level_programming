3089b771bf438b856c4a4ad5fd86da91fa89593d
LeighAnne17 <leighndimande17@icloud.com>
2024-07-31 15:19:04 -0700

    web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
-// Script that reads and prints the content of a file
-let fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function read (err, data) {
-  if (err) {
-    console.log(err);
-  } else {
+
+const fs = require('fs');
+const filePath = process.argv[2]; // First argument is the file path
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
     console.log(data);
-  }
 });
