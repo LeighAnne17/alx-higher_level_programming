b42f0f3d941d66b3e05fb20ad1fec56b953141ef
LeighAnne17 <leighndimande17@icloud.com>
2024-07-31 15:37:12 -0700

    web scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,28 +1,46 @@
 #!/usr/bin/node
 // Script that prints all characters of a Star Wars movie:
-let request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
-request.get(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    let films = JSON.parse(body);
-    let characterList = [];
-    for (let character of films.characters) {
-      characterList.push(new Promise(function (resolve, reject) {
-        request.get(character, function (err, response, body) {
-          if (err) {
-            console.log(err);
-          } else if (response.statusCode === 200) {
-            resolve(JSON.parse(body).name);
-          } else { reject(Error('Unknown')); }
-        });
-      }));
+
+const request = require('request');
+
+const movieId = process.argv[2]; // Movie ID from the command line argument
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request.get(url, (err, response, body) => {
+    if (err) {
+        console.error(err);
+        return;
     }
-    Promise.all(characterList).then(function (names) {
-      names.forEach(function (name) {
-        console.log(name);
-      });
-    });
-  }
+
+    try {
+        const data = JSON.parse(body);
+        const characterUrls = data.characters;
+
+        // Function to print character names in order
+        function fetchCharacterName(index) {
+            if (index >= characterUrls.length) return;
+
+            request.get(characterUrls[index], (err, response, body) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+
+                try {
+                    const character = JSON.parse(body);
+                    console.log(character.name);
+                    // Fetch the next character
+                    fetchCharacterName(index + 1);
+                } catch (parseError) {
+                    console.error('Error parsing JSON:', parseError);
+                }
+            });
+        }
+
+        // Start fetching characters
+        fetchCharacterName(0);
+
+    } catch (parseError) {
+        console.error('Error parsing JSON:', parseError);
+    }
 });
