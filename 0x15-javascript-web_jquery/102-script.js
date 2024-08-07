e4ad4a4fb5f7b8c57de538f43d7a3345bb3a34bb
LeighAnne17 <leighndimande17@icloud.com>
2024-08-07 13:31:15 -0700

    web query

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('#btn_translate').click(function() {
+    const langCode = $('#language_code').val();
+    $.get(`https://www.fourtonfish.com/hellosalut/hello/?lang=${langCode}`, function(data) {
+      $('#hello').text(data.hello);
+    }).fail(function() {
+      $('#hello').text('Error: Unable to fetch translation.');
+    });
+  });
+});
+
